d2308cfcd20e081e657b29ae76f9d9125e7a0e9e
J Pratt <jp10010101010000@gmail.com>
2022-08-25 15:49:59 +1000

    Add numbers and operators

--- src/assertions.rs
+++ src/assertions.rs
@@ -6,3 +6,8 @@ pub fn assert_is_err<T: std::fmt::Debug, E>(value: Result<T, E>) -> E {
     }
 }
 
+pub fn assert_err_is<T: std::fmt::Debug, E: std::fmt::Display>(value: Result<T, E>, err_msg: &str) {
+  let err = assert_is_err(value);
+  assert_eq!(format!("{}", err), err_msg);
+}
+
--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,6 @@
 use nom::{
   IResult,
-  bytes::complete::{tag, take_while, take_while_m_n},
+  bytes::complete::{tag, take_while, take_while1, take_while_m_n},
   combinator::map_res,
   branch::alt,
   character::complete::alpha1,
@@ -32,30 +32,52 @@ pub fn hex_color(input: &str) -> IResult<&str, Color> {
   Ok((input, Color { red, green, blue }))
 }
 
-fn is_symbol_char(c: char) -> bool {
+pub fn number_i64(input: &str) -> IResult<&str, i64> {
+  let (input, sign) = alt((tag("+"), tag("-"), tag("")))(input)?;
+  let (input, value) = map_res(
+    take_while1(&|c: char| c.is_digit(10)),
+    &|input| i64::from_str_radix(input, 10)
+  )(input)?;
+  Ok((input, if sign == "-" {
+      -value
+  } else {
+      value
+  }))
+}
+
+fn is_identifier_char(c: char) -> bool {
   c.is_alphanumeric() || (c == '_')
 }
 
-fn symbol_head(input: &str) -> IResult<&str, &str> {
+fn identifier_head(input: &str) -> IResult<&str, &str> {
   alt((alpha1, tag("_")))(input)
 }
 
-fn symbol_tail(input: &str) -> IResult<&str, &str> {
-  take_while(is_symbol_char)(input)
+fn identifier_tail(input: &str) -> IResult<&str, &str> {
+  take_while(is_identifier_char)(input)
 }
 
 pub fn symbol(og_input: &str) -> IResult<&str, Symbol> {
-  let (input, (head, tail)) = tuple((symbol_head, symbol_tail))(og_input)?;
+  let (input, (head, tail)) = tuple((identifier_head, identifier_tail))(og_input)?;
 
   let name = &og_input[0..head.len()+tail.len()];
 
   Ok((input, Symbol { name }))
 }
 
+fn is_operator_char(c: char) -> bool {
+  "~!@$%^&*-+=|?/\\:".contains(c)
+}
+
+pub fn operator(input: &str) -> IResult<&str, Symbol> {
+  let (input, name) = take_while_m_n(1, 3, is_operator_char)(input)?;
+  Ok((input, Symbol { name }))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::assertions::assert_is_err;
+    use crate::assertions::{assert_err_is, assert_is_err};
 
     #[test]
     fn parse_color() {
@@ -83,14 +105,37 @@ mod test {
       assert_eq!(symbol("_e_llo"), Ok(("", Symbol { name: "_e_llo" })));
     }
 
-    fn assert_err_is<T: std::fmt::Debug, E: std::fmt::Display>(value: Result<T, E>, err_msg: &str) {
-      let err = assert_is_err(value);
-      assert_eq!(format!("{}", err), err_msg);
-    }
-
     #[test]
     fn parse_non_symbol() {
       assert_err_is(symbol("#lol"), "Parsing Error: Error { input: \"#lol\", code: Tag }");
       assert_err_is(symbol("123"), "Parsing Error: Error { input: \"123\", code: Tag }");
     }
+
+    #[test]
+    fn parse_operator() {
+      assert_eq!(operator("||"), Ok(("", Symbol { name: "||" })));
+      assert_eq!(operator("+"), Ok(("", Symbol { name: "+" })));
+      assert_eq!(operator("*"), Ok(("", Symbol { name: "*" })));
+    }
+
+    #[test]
+    fn parse_non_operator() {
+      assert_err_is(operator("#lol"), "Parsing Error: Error { input: \"#lol\", code: TakeWhileMN }");
+      assert_err_is(operator("123"), "Parsing Error: Error { input: \"123\", code: TakeWhileMN }");
+    }
+
+    #[test]
+    fn parse_number_i64() {
+      assert_eq!(number_i64("123"), Ok(("", 123i64)));
+      assert_eq!(number_i64("-0"), Ok(("", 0i64)));
+      assert_eq!(number_i64("-1"), Ok(("", -1i64)));
+    }
+
+    #[test]
+    fn parse_non_number_i64() {
+      assert_err_is(number_i64("#lol"), "Parsing Error: Error { input: \"#lol\", code: TakeWhile1 }");
+      assert_err_is(number_i64("||"), "Parsing Error: Error { input: \"||\", code: TakeWhile1 }");
+      assert_err_is(number_i64("e2"), "Parsing Error: Error { input: \"e2\", code: TakeWhile1 }");
+    }
+
 }
