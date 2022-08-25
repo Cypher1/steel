6f98972ffd9e753eae81c5e30ea4f618a330229b
J Pratt <jp10010101010000@gmail.com>
2022-08-25 18:13:59 +1000

    Rename test arenas to ctx

--- src/ast.rs
+++ src/ast.rs
@@ -56,58 +56,76 @@ mod test {
 
     #[test]
     fn can_construct_node() {
-        let mut a: Arena<Node<'static>> = Arena::new();
+        let mut ctx: Arena<Node<'static>> = Arena::new();
 
-        let hello = a.add(Symbol::new("hello"));
+        let hello = ctx.add(Symbol::new("hello"));
 
         assert_eq!(
-            format!("{:?}", a.get(hello)),
+            format!("{:?}", ctx.get(hello)),
             "Ok(Symbol(Symbol { name: \"hello\" }))"
         );
     }
 
     #[test]
     fn can_construct_nodes() {
-        let mut a: Arena<Node<'static>> = Arena::new();
+        let mut ctx: Arena<Node<'static>> = Arena::new();
 
-        let hello = a.add(Symbol::new("hello"));
-        let world = a.add(Symbol::new("world"));
+        let hello = ctx.add(Symbol::new("hello"));
+        let world = ctx.add(Symbol::new("world"));
 
         assert_eq!(
-            format!("{:?}", a.get(hello)),
+            format!("{:?}", ctx.get(hello)),
             "Ok(Symbol(Symbol { name: \"hello\" }))"
         );
         assert_eq!(
-            format!("{:?}", a.get(world)),
+            format!("{:?}", ctx.get(world)),
             "Ok(Symbol(Symbol { name: \"world\" }))"
         );
     }
 
     #[test]
     fn can_construct_nodes_with_self_reference() {
-        let mut a: Arena<Node<'static>> = Arena::new();
+        let mut ctx: Arena<Node<'static>> = Arena::new();
 
-        let reference = a.add_with_id(|id| Call::new(id, vec![]));
+        let reference = ctx.add_with_id(|id| Call::new(id, vec![]));
 
         assert_eq!(
-            format!("{:?}", a.get(reference)),
+            format!("{:?}", ctx.get(reference)),
             format!("Ok(Call(Call {{ callee: {:?}, args: [] }}))", reference)
         );
     }
 
     #[test]
     fn can_construct_nodes_with_cross_reference() {
-        let mut a: Arena<Node<'static>> = Arena::new();
+        let mut ctx: Arena<Node<'static>> = Arena::new();
 
-        let hello = a.add(Symbol::new("hello"));
-        let world = a.add(Symbol::new("world"));
-        let reference = a.add(Call::new(hello, vec![world]));
+        let hello = ctx.add(Symbol::new("hello"));
+        let world = ctx.add(Symbol::new("world"));
+        let reference = ctx.add(Call::new(hello, vec![world]));
 
         assert_eq!(
-            format!("{:?}", a.get(reference)),
+            format!("{:?}", ctx.get(reference)),
             format!(
                 "Ok(Call(Call {{ callee: {:?}, args: [{:?}] }}))",
                 hello, world
+            )
+        );
+    }
+
+    #[test]
+    fn can_construct_values() {
+        let mut ctx: Arena<Node<'static>> = Arena::new();
+
+        let plus = ctx.add(Symbol::new("plus"));
+        let a = ctx.add(32i64);
+        let b = ctx.add(12i64);
+        let reference = ctx.add(Call::new(plus, vec![a, b]));
+
+        assert_eq!(
+            format!("{:?}", ctx.get(reference)),
+            format!(
+                "Ok(Call(Call {{ callee: {:?}, args: [{:?}, {:?}] }}))",
+                plus, a, b
             )
         );
     }
