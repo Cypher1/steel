d3238d96fd3cd40101f702d719c4c0bb329ca748
J Pratt <jp10010101010000@gmail.com>
2022-09-21 21:38:09 +1000

    Fixed random program generation

--- benches/random_programs.rs
+++ benches/random_programs.rs
@@ -33,18 +33,19 @@ fn generate_random_program<'b, T: CompilerContext<'b>>(
     size: usize,
     rng: &mut ThreadRng,
 ) -> T::ID {
-    if size > 2 {
-        let mut args_size: usize = rng.gen_range(0..size-2);
-        let num_args: usize = rng.gen_range(0..=args_size);
+    if size > 1 {
         let mut args = vec![];
-        for _ in 0..num_args {
-            let arg_size: usize = rng.gen_range(0..=args_size);
-            args_size -= arg_size;
-            if args_size > 0 {
-                args.push(generate_random_program(_name, store, args_size, rng));
+        let mut args_size: usize = rng.gen_range(1..size);
+        let num_inner: usize = size - args_size - 1;
+        if args_size > 0 {
+            let num_args: usize = rng.gen_range(1..=args_size);
+            args_size -= num_args; // at lesst one node per arg.
+            for i in 0..num_args {
+                let arg_size: usize = rng.gen_range(1..=1+args_size);
+                args_size -= arg_size-1;
+                args.push(generate_random_program(_name, store, arg_size, rng));
             }
         }
-        let num_inner: usize = size - num_args - 1;
         let callee = generate_random_program(_name, store, num_inner, rng);
         return store.add(Call {callee, args});
     }
