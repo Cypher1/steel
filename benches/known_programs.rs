370b185226f58d1c0165dc358fa4d46ad8dcb45d
J Pratt <jp10010101010000@gmail.com>
2022-10-20 08:55:13 +1100

    Generate large known programs

--- benches/known_programs.rs
+++ benches/known_programs.rs
@@ -4,19 +4,30 @@ use steel::{ast, ecs, gen_code::Spec };
 mod benchmark_types;
 use benchmark_types::*;
 
-const PROGRAMS: &[(usize, &'static str)] = &[
-    (1, "123"),
-    (10, "1+2+3+4+5"),
-    (10, "1*2*3*4*5"),
-    (20, "1+2+3+4+5+6+7+8+9+10+11+12+13+14+15+16+17+18+19+20"),
-    (20, "1*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20"),
-];
-
 fn criterion_benchmark(c: &mut Criterion) {
     let _ = env_logger::builder().is_test(true).try_init();
 
-    for (size, program) in PROGRAMS {
-        let spec = Spec::default().sized(*size);
+    let mut programs: Vec<(usize, String)> = vec![
+        (1, "123".to_string()),
+    ];
+
+    let mut plus_chain = "1".to_string();
+    let mut mul_chain = "1".to_string();
+    let mut size = 1;
+    let mut last = 1;
+    for i in 0..500 {
+        plus_chain = format!("{}+{}", i, plus_chain);
+        mul_chain = format!("{}+{}", i, mul_chain);
+        size += 3; // 1 = op, 1 = value i, 1 = the call.
+        if size > 10*last {
+            last = size;
+        }
+        programs.push((size, plus_chain.clone()));
+        programs.push((size, mul_chain.clone()));
+    }
+
+    for (size, program) in programs {
+        let spec = Spec::default().sized(size);
         let bench_type = format!("known program {}: {}", render_size(&spec), program);
         benchmarks::<ast::Ast>("ast", &bench_type, &program, &spec, c);
         benchmarks::<ecs::Ecs>("ecs", &bench_type, &program, &spec, c);
