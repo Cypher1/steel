84139ce57654d69ecf61c69588d5b3a69904d094
J Pratt <jp10010101010000@gmail.com>
2022-10-20 09:02:35 +1100

    Generate more bench marks for known programs

--- benches/known_programs.rs
+++ benches/known_programs.rs
@@ -1,5 +1,6 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use steel::{ast, ecs, gen_code::Spec };
+use log::trace;
 
 mod benchmark_types;
 use benchmark_types::*;
@@ -7,8 +8,8 @@ use benchmark_types::*;
 fn criterion_benchmark(c: &mut Criterion) {
     let _ = env_logger::builder().is_test(true).try_init();
 
-    let mut programs: Vec<(usize, String)> = vec![
-        (1, "123".to_string()),
+    let mut programs: Vec<(usize, String, String)> = vec![
+        (1, "known program constant".to_string(), "123".to_string()),
     ];
 
     let mut plus_chain = "1".to_string();
@@ -16,19 +17,22 @@ fn criterion_benchmark(c: &mut Criterion) {
     let mut size = 1;
     let mut last = 1;
     for i in 0..500 {
-        plus_chain = format!("{}+{}", i, plus_chain);
-        mul_chain = format!("{}+{}", i, mul_chain);
+        plus_chain = format!("{}+{}", i+2, plus_chain);
+        mul_chain = format!("{}*{}", i+2, mul_chain);
         size += 3; // 1 = op, 1 = value i, 1 = the call.
-        if size > 10*last {
+        if size >= (10*last) {
             last = size;
+            let spec = Spec::default().sized(size);
+            let bench_type = format!("known program {}: {}", render_size(&spec), "plus chain");
+            programs.push((size, bench_type, plus_chain.clone()));
+            let bench_type = format!("known program {}: {}", render_size(&spec), "mul chain");
+            programs.push((size, bench_type, mul_chain.clone()));
         }
-        programs.push((size, plus_chain.clone()));
-        programs.push((size, mul_chain.clone()));
     }
 
-    for (size, program) in programs {
+    for (size, bench_type, program) in programs {
+        trace!("Generating benchmark of size {}. {}. Program:\n{}", size, bench_type, program);
         let spec = Spec::default().sized(size);
-        let bench_type = format!("known program {}: {}", render_size(&spec), program);
         benchmarks::<ast::Ast>("ast", &bench_type, &program, &spec, c);
         benchmarks::<ecs::Ecs>("ecs", &bench_type, &program, &spec, c);
     }
