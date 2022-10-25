55db5e699c65fd46931a1377747ad0b00ec7fdf6
J Pratt <jp10010101010000@gmail.com>
2022-10-25 21:25:55 +1100

    Clean up

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -1,7 +1,6 @@
 use crate::compiler_context::CompilerContext;
 use crate::nodes::Operator;
 // use log::{debug, trace};
-use std::sync::atomic::{AtomicBool, Ordering::Relaxed};
 
 #[derive(Default, Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
 #[non_exhaustive]
@@ -26,11 +25,11 @@ impl Optimizations {
     }
 }
 
-fn constant_folding<C: CompilerContext + ?Sized>(
+fn constant_folding<C: CompilerContext + ?Sized + std::fmt::Debug>(
     context: &mut C,
     replace: &mut Vec<(C::ID, i64)>,
     root: C::ID,
-    fixed_point: &AtomicBool,
+    fixed_point: &mut bool,
 ) -> Result<C::ID, C::E> {
     // Find nodes to replace
     // ECS will run the Call component, but AST has to traverse all the nodes to check if they
@@ -60,30 +59,15 @@ fn constant_folding<C: CompilerContext + ?Sized>(
             return;
         };
         use Operator::*;
-        let result = match (name, left, right) {
-            // Both known
-            (Add, left, right) => left.wrapping_add(right),
-            (Sub, left, right) => left.wrapping_sub(right),
-            (Mul, left, right) => left.wrapping_mul(right),
-            (Div, left, right) => left.wrapping_div(right),
-            // One known (other discarded): Only sound if no side-effects...
-            // (Mul, Some(0), _) | (Mul, _, Some(0)) => 0,
-            // (Div, Some(0), _) => 0,
-            // (Div, _, Some(0)) => 0, // TODO: Error values (/0)
-            // One known (reduces to remaining)
-            /*
-            (Add, Some(0), Some(_zero_id), _, Some(value_id)) | (Add, _, Some(value_id), Some(0), Some(_zero_id)) => todo!(), // value_id
-            (Sub, Some(0), Some(_zero_id), _, Some(value_id)) => todo!(), // -*value_id
-            (Sub, _, Some(value_id), Some(0), Some(_zero_id)) => todo!(), // value_id
-            (Mul, Some(1), Some(_zero_id), _, Some(value_id)) | (Mul, _, Some(value_id), Some(1), Some(_zero_id)) => todo!(), // value_id
-            (Mul, _, Some(value_id), Some(1), Some(_zero_id)) => todo!(), // value_id
-            _ => return,
-            */
+        let result = match name {
+            Add => left.wrapping_add(right),
+            Sub => left.wrapping_sub(right),
+            Mul => left.wrapping_mul(right),
+            Div => left.wrapping_div(right),
         };
         // Update so that we don't have to re-find the updated values
         replace.push((id, result));
-        fixed_point.store(false, Relaxed);
-        // i64_value.shared.optimizer_data.value = Some(i64_value.value);
+        *fixed_point = false;
     })?;
     for (id, value) in replace.iter() {
         // println!("REPLACING {:?} with value: {}", id, value);
@@ -93,25 +77,25 @@ fn constant_folding<C: CompilerContext + ?Sized>(
     Ok(root)
 }
 
-pub fn optimize<C: CompilerContext + ?Sized>(
+pub fn optimize<C: CompilerContext + ?Sized + std::fmt::Debug>(
     context: &mut C,
     optimizations: &Optimizations,
     mut root: C::ID,
 ) -> Result<C::ID, C::E> {
     // Replace nodes
-    let fixed_point = AtomicBool::new(true);
+    let mut fixed_point;
     let mut replace: Vec<(C::ID, i64)> = Vec::new();
     loop {
-        fixed_point.store(true, Relaxed);
+        fixed_point = true;
         if optimizations.constant_folding {
             root = constant_folding(
                 context,
                 &mut replace,
                 root,
-                &fixed_point,
+                &mut fixed_point,
             )?;
         }
-        if fixed_point.load(Relaxed) {
+        if fixed_point {
             break;
         }
     }
