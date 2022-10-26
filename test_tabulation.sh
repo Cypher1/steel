ce2b233eae949c16a59b7d6991d51ff32fad2e80
J Pratt <jp10010101010000@gmail.com>
2022-10-27 00:01:02 +1100

    Improve tabulation output

--- test_tabulation.sh
+++ test_tabulation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat out | ./tabulate_bench_data.sh > tabulated
-cat tabulated | column -t -s ','
+cat out | ./tabulate_bench_data.sh | sed "s/optimize known program //" | sort -n | sed "s/^/optimize known program /" > tabulated
+cat tabulated | cut -d',' -f 1,3,6 | column -t -s ','
