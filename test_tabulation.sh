97858f065bddfb05f39a056cf16e3bd92200e98b
J Pratt <jp10010101010000@gmail.com>
2022-10-26 19:40:24 +1100

    Sort tabulated data

--- test_tabulation.sh
+++ test_tabulation.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat out | ./tabulate_bench_data.sh > tabulated
+cat out | ./tabulate_bench_data.sh | sed "s/optimize known program //" | sort -n | sed "s/^/optimize known program /" > tabulated
 cat tabulated | column -t -s ','
